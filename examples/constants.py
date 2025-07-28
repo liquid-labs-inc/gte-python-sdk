283801d8148dcd7c95ab6881ad59118b0f8c02ad
watrgoat <olivermatthew601@gmail.com>
2025-07-28 13:18:32 -0400

    add market/token constants to examples

---
+++ examples/constants.py
@@ -0,0 +1,9 @@
+from eth_utils.address import to_checksum_address
+
+BTC_USD_CLOB = to_checksum_address("0x0f3642714b9516e3d17a936baced4de47a6ffa5f")
+ETH_USD_CLOB = to_checksum_address("0x5ca9f32d4ce7cc0f782213c446c2ae14b754a623")
+BTC_ETH_AMM = to_checksum_address("0x6bba3ff4b359392decd0b77239b7e795269fb550")
+
+# Token addresses
+BTC_ADDRESS = to_checksum_address("0x7f11aa697e05b75600354ac9acf8bb209225e932")
+LAUNCHPAD_TOKEN_ADDRESS = to_checksum_address("0x0eacb3fb580973bb37786942b24fe0022403080f")
